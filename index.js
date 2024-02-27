f708702076d4cee2c1cc9ce8083954b307626998
Vision Liaka <113586539+ThatVishL@users.noreply.github.com>
2024-02-27 22:36:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$("body").css("backgroundColor","#cccc99");
+
+$(".blog").css({
+  "fontFamily":"cursive",
+  "marginTop":"10px",
+});
+
+$("#title").css({
+  "fontSize" : "4rem",
+  "textAlign" : "center",
+});
+
+$(".date p").css({
+  "fontSize":"2rem",
+  "marginLeft" : "10px",
+})
+
+$("h2").css({
+  "fontFamily" : "Courier New",
+  "fontSize" : "2rem",
+  "marginBottom": "10px",
+})
+
+$("footer p").css({
+  "textAlign" : "center",
+});
